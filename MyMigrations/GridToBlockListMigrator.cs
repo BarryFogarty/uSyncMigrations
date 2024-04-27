13b5fdeb52435c749068773071c36736ef698a7a
Barry Fogarty <barryfogarty@gmail.com>
2024-04-27 16:42:50 +0100

    Generate datatype config for sidebar BlockList

--- MyMigrations/GridToBlockListMigrator.cs
+++ MyMigrations/GridToBlockListMigrator.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.PropertyEditors;
+
 using uSync.Migrations.Core.Context;
+using uSync.Migrations.Core.Legacy.Grid;
 using uSync.Migrations.Core.Migrators;
 using uSync.Migrations.Core.Migrators.Models;
+
 
 namespace MyMigrations;
 
@@ -15,17 +19,21 @@ namespace MyMigrations;
 internal class GridToBlockListMigrator : SyncPropertyMigratorBase
 {
     private readonly ILogger<GridToBlockListMigrator> _logger;
+    private readonly ILegacyGridConfig _gridConfig;
+    private readonly ILoggerFactory _loggerFactory;
 
-    public GridToBlockListMigrator(ILogger<GridToBlockListMigrator> logger)
+    public GridToBlockListMigrator(
+        ILegacyGridConfig gridConfig,
+        ILoggerFactory loggerFactory,
+        ILogger<GridToBlockListMigrator> logger)
     {
+        _gridConfig = gridConfig;
+        _loggerFactory = loggerFactory;
         _logger = logger;
     }
 
     public override string GetEditorAlias(SyncMigrationDataTypeProperty propertyModel, SyncMigrationContext context)
         => Umbraco.Cms.Core.Constants.PropertyEditors.Aliases.BlockList;
-
-    public override string GetDatabaseType(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
-        => nameof(ValueStorageType.Ntext);
 
     // TODO: Convert the grid config to block list grid. 
     public override object? GetConfigValues(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
@@ -45,15 +53,124 @@ internal class GridToBlockListMigrator : SyncPropertyMigratorBase
             return new BlockListConfiguration();
         }
 
-        var gridConfiguration = JsonConvert
-            .DeserializeObject<GridConfiguration>(dataTypeProperty.ConfigAsString);
+        var gridConfiguration = JsonConvert.DeserializeObject<GridConfiguration>(dataTypeProperty.ConfigAsString);
 
-        // TODO: Make a blocklist config based on the grid datatype config (i.e. add the allowed blocks / settings etc
+        // TODO: Make a blocklist config based on the grid datatype config (i.e. add the allowed blocks etc)
 
-        return new BlockListConfiguration();
+        var legacyGridEditorsConfig = GetGridConfig(context);
+
+        // Default config
+        var config = new BlockListConfiguration()
+        {
+            ValidationLimit = new BlockListConfiguration.NumberRange
+            {
+                Max = null,
+                Min = null
+            },
+        };
+
+        switch (dataTypeProperty.DataTypeAlias)
+        {
+            case "Right Column Layout - Grid":
+                config.Blocks = AddSidebarBlocks(context);
+                break;
+            case "Banded Article":
+                config.Blocks = AddArticleBlocks(context);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException($"Unknown Grid datatype encountered: {dataTypeProperty.DataTypeAlias}");
+        }
+
+        return config;
+    }
+
+    private BlockListConfiguration.BlockConfiguration[] AddSidebarBlocks(SyncMigrationContext context)
+    {
+        // Would be best to get this from config, but for now let's just gen a hardcoded list of blocks
+        // TODO:  Detect / warn for any _Content_ blocks that do not have a matching config item
+        var blocks = new List<BlockListConfiguration.BlockConfiguration>
+        {
+            new() {
+                Label = "Links",
+                Thumbnail = "icon-link",
+                ContentElementTypeKey = context.ContentTypes.GetKeyByAlias("links")
+            },
+            new() {
+                Label = "Useful Links",
+                Thumbnail = "icon-link",
+                ContentElementTypeKey = context.ContentTypes.GetKeyByAlias("usefulLinks")
+            },
+            new() {
+                Label = "Useful Links (from Library)",
+                Thumbnail = "icon-link",
+                ContentElementTypeKey = context.ContentTypes.GetKeyByAlias("usefulLinksFromLibrary")
+            },
+            new() {
+                Label = "Social Media",
+                Thumbnail = "icon-share",
+                ContentElementTypeKey = context.ContentTypes.GetKeyByAlias("socialMedia")
+            },
+            new() {
+                Label = "Custom Social Media",
+                Thumbnail = "icon-share",
+                ContentElementTypeKey = context.ContentTypes.GetKeyByAlias("customSocialMedia")
+            },
+            new() {
+                Label = "RC Event",
+                Thumbnail = "icon-calendar",
+                ContentElementTypeKey = context.ContentTypes.GetKeyByAlias("rCEvent")
+            },
+            new() {
+                Label = "Event (from library)",
+                Thumbnail = "icon-calendar",
+                ContentElementTypeKey = context.ContentTypes.GetKeyByAlias("eventFromLibrary")
+            },
+            new() {
+                Label = "News Events Blogs",
+                Thumbnail = "icon-newspaper",
+                ContentElementTypeKey = context.ContentTypes.GetKeyByAlias("newsEventsBlogs")
+            },
+            new() {
+                Label = "Quotes",
+                Thumbnail = "icon-quote",
+                ContentElementTypeKey = context.ContentTypes.GetKeyByAlias("quotes")
+            }
+        };
+
+        return blocks.ToArray();
+    }
+
+    private BlockListConfiguration.BlockConfiguration[] AddArticleBlocks(SyncMigrationContext context)
+    {
+        var blocks = new List<BlockListConfiguration.BlockConfiguration>();
+
+        //foreach (var item in configuration)
+        //{
+        //    var alias = context.ContentTypes.GetReplacementAlias(item);
+
+        //    var contentTypeKey = context.ContentTypes.GetKeyByAlias(alias);
+
+        //    // tell the process we need this to be an element type
+        //    context.ContentTypes.AddElementTypes(new[] { contentTypeKey }, true);
+
+        //    blocks.Add(new BlockListConfiguration.BlockConfiguration
+        //    {
+        //        ContentElementTypeKey = contentTypeKey,
+        //        Label = item // TODO, add labels to sidebarGridEditorAliases (key value pair / dict)
+        //    });
+        //}
+
+        return blocks.ToArray();
     }
 
     // TODO: Convert grid content to blocklist grid content
     public override string? GetContentValue(SyncMigrationContentProperty contentProperty, SyncMigrationContext context)
         => base.GetContentValue(contentProperty, context);
+
+    private ILegacyGridEditorsConfig GetGridConfig(SyncMigrationContext context)
+    {
+        return _gridConfig.EditorsByContext(context);
+    }
+
+
 }
