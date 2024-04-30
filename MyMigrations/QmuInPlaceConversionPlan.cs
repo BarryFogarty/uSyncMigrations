cbb09707b6bc622f7951fc1a2d5c809cf419a2d3
Barry Fogarty <barryfogarty@gmail.com>
2024-04-30 15:21:52 +0100

    Add an in-place conversion plan
    This should avoid the need for the initial v8 Packing step - we do not need sitefiles now

--- MyMigrations/QmuInPlaceConversionPlan.cs
+++ MyMigrations/QmuInPlaceConversionPlan.cs
@@ -10,33 +10,33 @@ namespace MyMigrations;
 ///  Uses our custom grid to BlockList migrator to migarte the 2 'active' legacy grids 
 ///  Also scaffolds a blocklist editor for 'bandedContent' that replaces each 'Image Left/Right' grid row with an empty 'Banded Image Block' element, for later content pop 
 /// </summary>
-public class QmuMigrationPlan : ISyncMigrationPlan
+public class QmuInPlaceConversionPlan : ISyncMigrationPlan
 {
     private readonly SyncMigrationHandlerCollection _migrationHandlers;
 
-    public QmuMigrationPlan(SyncMigrationHandlerCollection migrationHandlers)
+    public QmuInPlaceConversionPlan(SyncMigrationHandlerCollection migrationHandlers)
     {
         _migrationHandlers = migrationHandlers;
     }
 
-    public string Name => "QMU Migration Plan";
+    public string Name => "QMU In-Place Conversion Plan";
 
     public string Icon => "icon-cloud color-blue";
 
-    public string Description => "A Custom Grid to BlockList migration with a couple of exclusions";
+    public string Description => "An in-place QMU Migration Plan variant with the same logic";
 
     public int Order => 10;
 
     public MigrationOptions Options => new()
     {
-        // TODO:  Enable as an in-place convert (?)
-        //Group = "Convert",
-        //Source = "uSync/v9",
+        // Enable as an in-place convert
+        Group = "Convert",
+        Source = "uSync/v9",
 
         SourceVersion = 8, // only run on v8 to v10+ migrations
 
         // write out to the same folder each time.
-        Target = $"{uSyncMigrations.MigrationFolder}/QMU-Migration",
+        //Target = $"{uSyncMigrations.MigrationFolder}/QMU-Migration",
 
         Handlers = _migrationHandlers.SelectGroup(8, string.Empty),
 
@@ -46,13 +46,7 @@ public class QmuMigrationPlan : ISyncMigrationPlan
         {
             { Umbraco.Cms.Core.Constants.PropertyEditors.Aliases.Grid, nameof(GridToBlockListMigrator) },
             { Umbraco.Cms.Core.Constants.PropertyEditors.Aliases.NestedContent, nameof(NestedToBlockListMigrator) }
-        },
-
-        // add a list of properties we are ignoring on all content
-        //IgnoredProperties = new List<string>
-        //{
-        //    "bandedContent"
-        //},
+        }
     };
 
 }
